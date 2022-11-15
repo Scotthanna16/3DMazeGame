03c8b938d24741c1dfc46cc9c974f80342e38d2e
William Scott Hanna <wshanna@wm.edu>
2022-11-15 13:29:15 -0500

    Winning and losing screens finished, button implemented

--- app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/LosingActivity.java
@@ -1,15 +1,38 @@
 package edu.wm.cs.cs301.ScottHanna.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.ScottHanna.R;
 
+import android.view.View;
+import android.widget.Button;
+
 public class LosingActivity extends AppCompatActivity {
+
+    private Button backbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.losingscreen);
+        backbutton=findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+
+            }
+        });
+    }
+
+    /**
+     * Changes the activity from losing screen to title screen
+     */
+    private void changeActivity(){
+        Intent intent=new Intent(this, AMazeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/WinningActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/WinningActivity.java
@@ -1,15 +1,37 @@
 package edu.wm.cs.cs301.ScottHanna.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.ScottHanna.R;
 
 public class WinningActivity extends AppCompatActivity {
+
+    private Button backbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.winningscreen);
+        backbutton=findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+
+            }
+        });
+    }
+
+    /**
+     * Changes the activity from losing screen to title screen
+     */
+    private void changeActivity(){
+        Intent intent=new Intent(this, AMazeActivity.class);
+        startActivity(intent);
     }
 }
