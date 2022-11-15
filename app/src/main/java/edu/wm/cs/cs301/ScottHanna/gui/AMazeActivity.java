67ef60f0c3e15d65bccc1d2cc51d8bec8590594c
William Scott Hanna <wshanna@wm.edu>
2022-11-15 18:59:49 -0500

    buttons implemented for title screen,change made to manifest

--- app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/AMazeActivity.java
@@ -1,15 +1,48 @@
 package edu.wm.cs.cs301.ScottHanna.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.ScottHanna.R;
 
 public class AMazeActivity extends AppCompatActivity {
+
+    private Button explore;
+    private Button revisit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mazetitle);
+        explore=findViewById(R.id.Explore);
+        revisit=findViewById(R.id.revisit);
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+
+            }
+        });
+        revisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+
+            }
+        });
+
+
+    }
+
+    /**
+     * Changes the activity from losing screen to title screen
+     */
+    private void changeActivity(){
+        Intent intent=new Intent(this, GeneratingActivity.class);
+        startActivity(intent);
     }
 }
