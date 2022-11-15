60ae2242813d91f58d5345c2ec1bf3aab4f0b8e2
William Scott Hanna <wshanna@wm.edu>
2022-11-15 12:53:51 -0500

    Starting to design title screen

---
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/AMazeActivity.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.cs301.ScottHanna.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.ScottHanna.R;
+
+public class AMazeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
