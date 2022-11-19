7a4aec3df784621e701bdb8d327a3bf8e8f61c25
William Scott Hanna <wshanna@wm.edu>
2022-11-19 13:00:38 -0500

    Graphics for Titlescreen done, spinner, seekbar implemented

--- app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/AMazeActivity.java
@@ -4,22 +4,88 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.ScottHanna.R;
 
-public class AMazeActivity extends AppCompatActivity {
+public class AMazeActivity extends AppCompatActivity{
 
     private Button explore;
     private Button revisit;
-
+    private String[] Algorithms={"DFS","Prim","Boruvka"};
+    private String[] rooms={"Rooms","No Rooms"};
+    SeekBar seekbar;
+    TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mazetitle);
+        seekbar= (SeekBar)findViewById(R.id.Seekbarid);
+        view=(TextView)findViewById(R.id.rating);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                view.setText("Rating"+String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         explore=findViewById(R.id.Explore);
         revisit=findViewById(R.id.revisit);
+        Spinner alg=(Spinner) findViewById(R.id.Algorithm);
+
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, Algorithms);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        alg.setAdapter(aa);
+        alg.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(AMazeActivity.this,Algorithms[i],Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(AMazeActivity.this,Algorithms[0],Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Spinner roomyn=(Spinner) findViewById(R.id.Rooms);
+
+        ArrayAdapter<String> a2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, rooms);
+        a2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roomyn.setAdapter(a2);
+        roomyn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(AMazeActivity.this,rooms[i],Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(AMazeActivity.this,rooms[0],Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
         explore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +104,7 @@ public class AMazeActivity extends AppCompatActivity {
 
     }
 
+
     /**
      * Changes the activity from losing screen to title screen
      */
@@ -45,4 +112,7 @@ public class AMazeActivity extends AppCompatActivity {
         Intent intent=new Intent(this, GeneratingActivity.class);
         startActivity(intent);
     }
+
+
+
 }
