04ee8ccb7ba903fa1f4ef6bf2ce9d139532b3d57
William Scott Hanna <wshanna@wm.edu>
2022-11-20 21:49:15 -0500

    Initial Bitmap figured out

--- app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/MazePanel.java
+++ app/src/main/java/edu/wm/cs/cs301/ScottHanna/gui/MazePanel.java
@@ -3,12 +3,15 @@ package edu.wm.cs.cs301.ScottHanna.gui;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-/*
+
 public class MazePanel extends View {
 
     Paint paint;
@@ -16,22 +19,54 @@ public class MazePanel extends View {
     Canvas canvas;
 
 
+
+
     public MazePanel(Context context) {
         super(context);
-        bmap=new
+        init();
+        canvas.drawRect(0,0,500,500,paint);
+
+
+
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        Log.v("Null testing", String.valueOf(bmap == null));
+        canvas.drawBitmap(bmap,0,0,paint);
+    }
+
+
+
+    public void init(){
+        bmap= Bitmap.createBitmap(500, 500, Bitmap.Config.RGB_565);
         canvas=new Canvas(bmap);
+        paint=new Paint();
+        paint.setColor(Color.BLACK);
     }
 
     public MazePanel(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+        canvas.drawRect(0,0,500,500,paint);
+
     }
 
     public MazePanel(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+        canvas.drawRect(0,0,500,500,paint);
+
     }
 
     public MazePanel(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+        canvas.drawRect(0,0,500,500,paint);
+
     }
 
-}*/
+}
